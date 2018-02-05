2870344567f5c44a4fdb126ef979cc13cb84fe0e
ravit <ravit@DESKTOP-7ELK956.home>
2018-02-04 19:20:50 -0500

    CucumberRun

---
+++ src/cognizantProject/CucumberRun.java
@@ -0,0 +1,14 @@
+package cognizantProject;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="D:\\Workplace\\cognizantProject\\features\\newegg.feature"
+)
+public class CucumberRun {
+
+}
